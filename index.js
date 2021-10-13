daf2006083606f82b08cd66bf2ea4982e92b7d63
Brent Graves <graves.brent@gmail.com>
2021-10-13 16:30:14 -0400

    added a list of questions to be prompted to ask for the readme generator.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// TODO: Include packages needed for this application
+// TODO: Include packages needed for this application to run 
 //fs comes with the core package
 const fs = require('fs');
 const path = require('path');
@@ -24,13 +24,93 @@ const generateMarkdown = require('.utils/generateMarkdown');
 // License to assign project
 // TODO: Create an array of questions for user input
 
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "What is your project's description?",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "What goals did you have when building your project?",
+        name: "goals",
+    },
+    {
+        type: "input",
+        message: "What motivated you to build this project",
+        name: "motivation",
+    },
+    {
+        type: "Input",
+        message: "What sets your project apart from other projects available?",
+        name: "uniqueness",
+    },
+    {
+        type: "Input",
+        message: "What are your project's installation instructions?",
+        name: "install",
+        default: "npm i",
+    },
+    {
+        type: "input",
+        message: "What is your project's intended use?",
+        name: "usage",
+    },
+    {
+        type: "input",
+        message: "What technologies does your project use?",
+        name: "tech",
+    },
+    {
+        type:"input",
+        message: "What problems did you face when designing this this project?",
+        name: "problems",
+    },
+    {
+        type: "input",
+        message: "What commands should be run to test your project?",
+        name: "tests",
+        default: "npm run test",
+    },
+    {
+        type: "input",
+        message: "Who needs credit on this project?",
+        name: "credits",
+    },
+    {
+        type: "input",
+        message: "How does someone contribute to this project?",
+        name: "contribute",
+    },
+    {
+        type: "input",
+        message: "What is your email?",
+        name: "contact",
+    },
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "github"
+    },
+    {
+        type: "list",
+        message: "How is this project licensed?",
+        name: "license",
+        choices: ["Apache", "Boost Software", "Creative Commons", "GNU", "ISC", "MIT", "Mozilla Public", "The Unlicense", "Other"],
+    },
+];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+//function init() {}
 
 // Function call to initialize app
 init();
