8d63ff425dfa3057f34aacdd9f55ea9ba1c54808
Brent Graves <graves.brent@gmail.com>
2021-10-13 16:17:46 -0400

    added a list of comments I am going to use to write my questions.

--- index.js
+++ index.js
@@ -5,7 +5,25 @@ const path = require('path');
 const inquirer = require('inquirer');
 const generateMarkdown = require('.utils/generateMarkdown');
 
+// list of comments to help me write my questions
+
+// Project Title
+// Description of Project
+// Project Goals
+// Motivation to Build Project
+// What makes this project different?
+// Project Installation
+// Projects Usage
+// Technologies used to design project
+// How to Test project
+// Problems encountered when designing project
+// What features does your project use
+// Credits for project
+// Collaborators on project
+// Contact information
+// License to assign project
 // TODO: Create an array of questions for user input
+
 const questions = [];
 
 // TODO: Create a function to write README file
